322cd20a110ce2d51f633eb34b592780cbcf9fb3
superiorshrimp <szymon.zychowicz300701@gmail.com>
2021-12-12 11:41:24 +0100

    parsing v1

---
+++ src/production.py
@@ -0,0 +1,6 @@
+class Production:
+    def __init__(self, name, left, right, morphisms):
+        self.name = name
+        self.left = left
+        self.right = right
+        self.morphisms = morphisms
